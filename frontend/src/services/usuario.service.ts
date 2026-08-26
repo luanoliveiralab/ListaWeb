81692c7ec5e8e2319807454cb3e2942bf500d53b
Luan Oliveira <luanoliveiralab@users.noreply.github.com>
2026-08-26 15:54:45 -0300

    chore: remover código e dependências não utilizados

--- frontend/src/services/usuario.service.ts
+++ frontend/src/services/usuario.service.ts
@@ -6,18 +6,10 @@ export const usuarioService = {
         return api.put(`/usuarios/${id}`, dados);
     },
 
-    buscarPorId(id: number) {
-        return api.get(`/usuarios/${id}`);
-    },
-
     alterarSenha(senhaAtual: string, novaSenha: string) {
         return api.put("/alterar-senha", {
             senhaAtual,
             novaSenha,
         });
-    },
-
-    excluir(id: number) {
-        return api.delete(`/usuarios/${id}`);
     },
 };
